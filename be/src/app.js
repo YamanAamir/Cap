f1808e841b0d412fed1b16002213200c77d82e41
yamanaamir <yamanaamir2004@gmail.com>
2026-05-15 20:58:41 +0500

    Update app.js

--- be/src/app.js
+++ be/src/app.js
@@ -19,9 +19,11 @@ app.post(
   webhookController.stripeWebhook
 );
 
-// ❗ ab baaki middleware
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
 app.use(cookieParser());
+
 app.use(cors({
   origin: true,
   credentials: true,
