deaebdcb86deec5cd6172c2fe1a44b3403ef2d57
Abdul Ahad <abdulahad010274@gmail.com>
2026-09-10 12:02:15 +0500

    ahad

--- caplivebackend/src/routes/sendEmail.routes.js
+++ caplivebackend/src/routes/sendEmail.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { workflowStatusChange, sendCapEmail, stripePayment, getSessionDetails, stripeWebhook, createInstallmentOrder, payInstallment } = require('../controllers/sendEmail.controller');
+const { workflowStatusChange, sendCapEmail, stripePayment, getSessionDetails, stripeWebhook, createInstallmentOrder, payInstallment, createDirectOrder } = require('../controllers/sendEmail.controller');
 // const upload = require('../middlewares/uploadOrderFiles');
 // const { authenticate } = require('../middlewares/auth.middleware');
 
 router.put('/workflow/:id', workflowStatusChange);
 router.post('/capconfigurator', sendCapEmail);
 router.post('/create-checkout-session', stripePayment);
+router.post('/create-direct-order', createDirectOrder);
 router.post('/create-installment-order', createInstallmentOrder);
 router.get('/pay-installment', payInstallment);
 router.get('/checkout-session', getSessionDetails);
