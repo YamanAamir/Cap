e0057fad0ae6c2b383269e4e63e3fb93a6e777fa
Abdul Ahad <abdulahad010274@gmail.com>
2026-09-10 13:19:49 +0500

    ghgh

--- caplivedashboard/src/pages/ProductionPage.jsx
+++ caplivedashboard/src/pages/ProductionPage.jsx
@@ -182,7 +182,7 @@ const ProductionPage = () => {
 
   const getBaseUrl = () => {
     if (window.location.hostname === 'localhost') return 'http://localhost:3000';
-    return 'https://caplivevapi.studentlife.dk';
+    return 'https://capliveapi.studentlife.dk';
   };
 
   return (
